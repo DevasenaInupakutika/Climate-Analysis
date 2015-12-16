49395dc0204bc85647aef8e90edc311a05104927
DevasenaInupakutika <devasena.prasad@gmail.com>
2015-12-16 14:59:09 +0000

    Added test script to temperature conversion module

---
+++ test_temp_conversion.py
@@ -0,0 +1,17 @@
+from temp_conversion import fahr_to_kelvin
+from nose.tools import *
+
+def test_basic_value():
+    assert fahr_to_kelvin(20.0)==266.4833333333333
+
+def test_zero_kelvin():
+    assert round(fahr_to_kelvin(-459.67),2)==0.00
+
+@raises(TypeError)
+def test_temp_string():
+    assert fahr_to_kelvin("SomeTemperature")
+ 
+
+@raises(TypeError)
+def test_null_temp():
+    assert fahr_to_kelvin()
